0fa6d88bfd80edbc25c4f0c00156c89c1667165a
Damian Kłos <alojzy231@gmail.com>
2023-07-31 15:51:59 +0200

    fix: trigger second build in ui package

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop1")}>Boop</button>;
+  return <button onClick={() => alert("boop2")}>Boop</button>;
 };
