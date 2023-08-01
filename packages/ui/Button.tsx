94f80250683777e813cc0db88eb0b4b5380f86bc
Damian Kłos <alojzy231@gmail.com>
2023-08-01 09:51:16 +0200

    feat: trigger release in package

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop4")}>Boop</button>;
+  return <button onClick={() => alert("boop5")}>Boop</button>;
 };
