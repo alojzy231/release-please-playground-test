6bc72facadcbf81a4cee29f10718fe39c5fafe34
Damian Kłos <alojzy231@gmail.com>
2023-08-01 10:40:53 +0200

    feat: trigger build in package

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop5")}>Boop</button>;
+  return <button onClick={() => alert("boop6")}>Boop</button>;
 };
